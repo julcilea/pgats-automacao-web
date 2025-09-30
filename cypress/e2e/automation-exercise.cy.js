bd8f9bbbe2717f8d9ae23c660cedb9ac06e163ea
Julcilea <julcilea@gmail.com>
2025-09-29 23:00:58 -0300

    Incluindo mais testes exercicio 1

--- cypress/e2e/automation-exercise.cy.js
+++ cypress/e2e/automation-exercise.cy.js
@@ -1,32 +1,45 @@
 /// <reference types="cypress" />
 
 describe('Automation exercise', () => {
-    it('Cadastrar um usuário', () => {
+    const tryLogout = () => {
+        // Verifica se existe o link de logout (usuário está logado)
+        cy.get('body').then($body => {
+            if ($body.find('a[href="/logout"]').length > 0) {
+                cy.get('a[href="/logout"]').click()
+                cy.url().should('include', '/login')
+            }
+        })
+    }
+
+    afterEach(() => {
+        // Tenta fazer logout após cada teste
+        tryLogout()
+    })
+
+    const generateTestUser = () => {
         const timestamp = new Date().getTime()
+        return {
+            name: 'QA Tester',
+            email: `qa-tester-${timestamp}@test.com`,
+            password: 'teste123'
+        }
+    }
 
-        cy.viewport(300, 720)
-        //cy.viewport('iphone-xr')
-
+    const createAccount = (user) => {
         cy.visit('https://automationexercise.com/')
         cy.get('a[href="/login"]').click()
-        cy.get('input[data-qa="signup-name"]').type('QA Tester')
-        cy.get('input[data-qa="signup-email"]').type(`qa-tester-${timestamp}@test.com`)
-        //cy.get('button[data-qa="signup-button"]').click()
+        cy.get('input[data-qa="signup-name"]').type(user.name)
+        cy.get('input[data-qa="signup-email"]').type(user.email)
         cy.contains('button', 'Signup').click()
 
         cy.get('input[type="radio"]').check('Mrs')
-        //cy.get('input[id="id_gender2"]').check()
-        cy.get('input[data-qa="password"]').type('teste123', { log: false }) //oculta a senha no log do cypress
-        //cy.get('input#password').type('teste123')
+        cy.get('input[data-qa="password"]').type(user.password, { log: false })
 
         cy.get('select[data-qa="days"]').select('21')
-        //cy.get('select#days').select('21')
         cy.get('select[data-qa="months"]').select('November')
-        //cy.get('data-qa=months]').select('November')
         cy.get('select[data-qa="years"]').select('1979')
 
         cy.get('input[type=checkbox]#newsletter').check()
-        //cy.get('input[type=newsletter').check()
         cy.get('input[type=checkbox]#optin').check()
 
         cy.get('input#first_name').type('QA')
@@ -41,13 +54,85 @@ describe('Automation exercise', () => {
         cy.get('input#mobile_number').type('+5511999999999')
 
         cy.get('button[data-qa="create-account"]').click()
-
         cy.url().should('include', 'account_created')
-        //cy.contains('Account Created!').should('be.visible')
+    }
+
+    it('Cadastrar um usuário', () => {
+        cy.viewport(300, 720)
+        const user = generateTestUser()
+        createAccount(user)
         cy.get('h2[data-qa="account-created"]').should('have.text', 'Account Created!')
     })
 
-    it('', () => {
+    it('Logar com um usuário existente', () => {
+        const user = generateTestUser()
+        createAccount(user)
+
+        cy.visit('https://automationexercise.com/')
+        tryLogout()
+        cy.get('a[href="/login"]').click()
+
+        // Login com usuário válido
+        cy.get('input[data-qa="login-email"]').type(user.email)
+        cy.get('input[data-qa="login-password"]').type(user.password, { log: false })
+        cy.get('button[data-qa="login-button"]').click()
+
+        // Verificar se o login foi bem sucedido
+        cy.get('a[href="/logout"]').should('be.visible')
+        cy.get('i.fa.fa-user').parent().should('contain', 'Logged in as')
+    })
+
+    it('Logar com um usuário inexistente', () => {
+        cy.visit('https://automationexercise.com/')
+        cy.get('a[href="/login"]').click()
+
+        // Tentar login com usuário inválido
+        cy.get('input[data-qa="login-email"]').type('usuario-inexistente@test.com')
+        cy.get('input[data-qa="login-password"]').type('senhaerrada123', { log: false })
+        cy.get('button[data-qa="login-button"]').click()
+
+        // Verificar mensagem de erro
+        cy.get('p[style="color: red;"]').should('have.text', 'Your email or password is incorrect!')
+    })
+
+    it('Fazer logout do usuário logado', () => {
+        const user = generateTestUser()
+        createAccount(user)
+
+        // Primeiro fazer login
+        cy.visit('https://automationexercise.com/')
+        tryLogout()
+        cy.get('a[href="/login"]').click()
+        cy.get('input[data-qa="login-email"]').type(user.email)
+        cy.get('input[data-qa="login-password"]').type(user.password, { log: false })
+        cy.get('button[data-qa="login-button"]').click()
+
+        // Verificar se está logado
+        cy.get('i.fa.fa-user').parent().should('contain', 'Logged in as')
+
+        // Fazer logout
+        cy.get('a[href="/logout"]').click()
+
+        // Verificar se voltou para a página de login
+        cy.url().should('include', '/login')
+        cy.get('div.login-form h2').should('have.text', 'Login to your account')
+    })
+
+    it('Cadastrar um usuário com email já cadastrado', () => {
+        const user = generateTestUser()
+        createAccount(user)
+
+        cy.visit('https://automationexercise.com/')
+        tryLogout()
+        cy.get('a[href="/login"]').click()
+
+        // Tentar cadastrar com email já existente
+        cy.get('input[data-qa="signup-name"]').type('QA Duplicado')
+        cy.get('input[data-qa="signup-email"]').type(user.email)
+        cy.contains('button', 'Signup').click()
+
+        // Verificar mensagem de erro
+        cy.get('p[style="color: red;"]').should('have.text', 'Email Address already exist!')
     })
 
 })
