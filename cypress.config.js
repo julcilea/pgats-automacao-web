1aab22db5acf3c6deaa396672fb0cf793c76817f
Julcilea <julcilea@gmail.com>
2025-10-18 10:22:10 -0300

    incluindo pipeline e ajuste teste arq

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 require('cypress-mochawesome-reporter/plugin');
 
 module.exports = defineConfig({
+  retries: {
+    "runMode": 1,
+    "openMode": 0
+  },
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
@@ -11,6 +15,7 @@ module.exports = defineConfig({
     saveAllAttempts: false,
   },
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
